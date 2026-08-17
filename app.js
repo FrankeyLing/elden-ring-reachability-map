0e4f63417efccb1eac18372d193faa3ac5dcac45
frankeyling <2493735273@qq.com>
2026-08-18 00:19:42 +0800

    feat: model Liurnia optional dungeons

--- app.js
+++ app.js
@@ -748,6 +748,12 @@ async function init() {
       ["Tombsward Cave", "grace_limgrave_weeping_peninsula_tombsward_cave"],
       ["Tombsward Catacombs", "grace_limgrave_weeping_peninsula_tombsward_catacombs"],
       ["Impaler's Catacombs", "grace_limgrave_weeping_peninsula_impaler_s_catacombs"],
+      ["Stillwater Cave", "grace_liurnia_of_the_lakes_main_stillwater_cave"],
+      ["Scenic Isle", "grace_liurnia_of_the_lakes_main_scenic_isle"],
+      ["Lakeside Crystal Cave", "grace_liurnia_of_the_lakes_main_lakeside_crystal_cave"],
+      ["Converted Tower", "grace_liurnia_of_the_lakes_main_converted_tower"],
+      ["Road's End Catacombs", "grace_liurnia_of_the_lakes_main_road_s_end_catacombs"],
+      ["Cliffbottom Catacombs", "grace_liurnia_of_the_lakes_main_cliffbottom_catacombs"],
       ["Stormhill Shack", "grace_stormhill_shack"],
       ["Stormveil Main Gate", "grace_stormveil_main_gate"],
       ["Stormveil Cliffside", "grace_stormveil_cliffside"],
@@ -874,6 +880,7 @@ async function init() {
       if (["Stormfoot Catacombs", "Limgrave Tunnels", "Groveside Cave", "Coastal Cave"].includes(record.name)) return;
       if (["Murkwater Coast", "Saintsbridge", "Murkwater Catacombs", "Murkwater Cave", "Deathtouched Catacombs", "Highroad Cave"].includes(record.name)) return;
       if (["South of the Lookout Tower", "Fourth Church of Marika", "Church of Pilgrimage", "Earthbore Cave", "Tombsward Cave", "Tombsward Catacombs", "Impaler's Catacombs"].includes(record.name)) return;
+      if (["Stillwater Cave", "Scenic Isle", "Lakeside Crystal Cave", "Converted Tower", "Road's End Catacombs", "Cliffbottom Catacombs"].includes(record.name)) return;
       if (["Sealed Tunnel", "Divine Tower of West Altus: Gate", "Divine Tower of West Altus"].includes(record.name)) return;
       if (["Avenue Balcony", "Erdtree Sanctuary", "Elden Throne", "Siofra River Well Depths", "Siofra River Bank", "Worshippers' Woods", "Below the Well", "Deep Siofra Well", "Crumbling Beast Grave Depths", "Dragon Temple Altar", "Castle Front", "Castle Ensis Checkpoint", "Castle-Lord's Chamber", "Ensis Moongazing Grounds", "Ainsel River Main", "Nokstella, Eternal City", "Nokstella Waterfall Basin", "Lake of Rot Shoreside", "Grand Cloister", "Moonlight Altar", "Palace Approach Ledge-Road", "Dynasty Mausoleum Entrance", "Dynasty Mausoleum Midpoint", "Cocoon of the Empyrean", "Grand Lift of Rold", "Zamor Ruins", "Whiteridge Road", "Freezing Lake", "Giants' Gravepost", "Foot of the Forge", "Fire Giant", "Forge of the Giants", "Castle Sol Main Gate", "Church of the Eclipse", "Castle Sol Rooftop", "Consecrated Snowfield", "Inner Consecrated Snowfield", "The First Step", "Church of Elleh", "Gatefront", "Stormhill Shack", "Mistwood Outskirts", "Third Church of Marika", "Siofra River Well", "Ancestral Woods", "Aqueduct-Facing Cliffs", "Stormveil Main Gate", "Stormveil Cliffside", "Rampart Tower", "Liftside Chamber", "Secluded Cell", "Lake-Facing Cliffs", "Main Academy Gate", "Church of the Cuckoo", "Schoolhouse Classroom", "Debate Parlor", "Raya Lucaria Grand Library", "Grand Lift of Dectus", "Altus Plateau", "Altus Highway Junction", "Outer Wall Phantom Tree", "Outer Wall Battleground", "East Capital Rampart", "Nokron, Eternal City", "Deeproot Depths", "Prince of Death's Throne", "Ordina, Liturgical Town", "Volcano Manor", "Prison Town Church", "Temple of Eiglay", "Haligtree Canopy"].includes(record.name)) return;
       const groupKey = `${record.layer}|${record.region}`;
